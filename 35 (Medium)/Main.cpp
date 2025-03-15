f0632369003aec7227ba8504b0047362b29e7685
Username <87919916+AP0tato@users.noreply.github.com>
2025-03-15 13:50:17 -0400

    stuff

---
+++ 35 (Medium)/Main.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <iostream>
+
+class Solution 
+{
+    public:
+    bool isValidSudoku(std::vector<std::vector<char>>& board) {
+        
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}

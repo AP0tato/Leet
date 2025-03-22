43743595fb46939c8d73d6ce9af8546092db4484
Username <87919916+AP0tato@users.noreply.github.com>
2025-03-22 01:01:33 -0400

    more problems

--- 37 (Hard)/Main.cpp
+++ 37 (Hard)/Main.cpp
@@ -2,130 +2,114 @@
 #include <string>
 #include <iostream>
 #include <algorithm>
+#include <map>
+#include <set>
+#include <time.h>
 
 using namespace std;
+
+void print(vector<vector<char>> &v);
 
 class Solution 
 {
     public:
-    bool isValidSudoku(vector<vector<char>>& board) 
-    {
-        vector<string> s;
-        for(vector<char> i : board)
-        {
-            string t(i.begin(), i.end());
-            string::difference_type n1 = count(t.begin(), t.end(), '1');
-            string::difference_type n2 = count(t.begin(), t.end(), '2');
-            string::difference_type n3 = count(t.begin(), t.end(), '3');
-            string::difference_type n4 = count(t.begin(), t.end(), '4');
-            string::difference_type n5 = count(t.begin(), t.end(), '5');
-            string::difference_type n6 = count(t.begin(), t.end(), '6');
-            string::difference_type n7 = count(t.begin(), t.end(), '7');
-            string::difference_type n8 = count(t.begin(), t.end(), '8');
-            string::difference_type n9 = count(t.begin(), t.end(), '9');
-
-            int c1 = static_cast<int>(n1);
-            int c2 = static_cast<int>(n2);
-            int c3 = static_cast<int>(n3);
-            int c4 = static_cast<int>(n4);
-            int c5 = static_cast<int>(n5);
-            int c6 = static_cast<int>(n6);
-            int c7 = static_cast<int>(n7);
-            int c8 = static_cast<int>(n8);
-            int c9 = static_cast<int>(n9);
-
-            if(c1>1||c2>1||c3>1||c4>1||c5>1||c6>1||c7>1||c8>1||c9>1)
-                return false;
-
-            s.push_back(t);
-        }
-
-        for(int i = 0; i < 9; i++)
-        {
-            string t; 
-            t.push_back(s[0][i]);
-            t.push_back(s[1][i]);
-            t.push_back(s[2][i]);
-            t.push_back(s[3][i]);
-            t.push_back(s[4][i]);
-            t.push_back(s[5][i]);
-            t.push_back(s[6][i]);
-            t.push_back(s[7][i]);
-            t.push_back(s[8][i]);
-
-            string::difference_type n1 = count(t.begin(), t.end(), '1');
-            string::difference_type n2 = count(t.begin(), t.end(), '2');
-            string::difference_type n3 = count(t.begin(), t.end(), '3');
-            string::difference_type n4 = count(t.begin(), t.end(), '4');
-            string::difference_type n5 = count(t.begin(), t.end(), '5');
-            string::difference_type n6 = count(t.begin(), t.end(), '6');
-            string::difference_type n7 = count(t.begin(), t.end(), '7');
-            string::difference_type n8 = count(t.begin(), t.end(), '8');
-            string::difference_type n9 = count(t.begin(), t.end(), '9');
-
-            int c1 = static_cast<int>(n1);
-            int c2 = static_cast<int>(n2);
-            int c3 = static_cast<int>(n3);
-            int c4 = static_cast<int>(n4);
-            int c5 = static_cast<int>(n5);
-            int c6 = static_cast<int>(n6);
-            int c7 = static_cast<int>(n7);
-            int c8 = static_cast<int>(n8);
-            int c9 = static_cast<int>(n9);
-
-            if(c1>1||c2>1||c3>1||c4>1||c5>1||c6>1||c7>1||c8>1||c9>1)
-                return false;
-        }
-
-        for(int i = 0; i < 3; i++)
-        {
-            for(int j = 0; j < 3; j++)
-            {
-                string t;
-                for(int k = 0; k < 3; k++)
-                {
-                    t.push_back(s[i*3][j*3+k]);
-                    t.push_back(s[i*3+1][j*3+k]);
-                    t.push_back(s[i*3+2][j*3+k]);
-                }
-                string::difference_type n1 = count(t.begin(), t.end(), '1');
-                string::difference_type n2 = count(t.begin(), t.end(), '2');
-                string::difference_type n3 = count(t.begin(), t.end(), '3');
-                string::difference_type n4 = count(t.begin(), t.end(), '4');
-                string::difference_type n5 = count(t.begin(), t.end(), '5');
-                string::difference_type n6 = count(t.begin(), t.end(), '6');
-                string::difference_type n7 = count(t.begin(), t.end(), '7');
-                string::difference_type n8 = count(t.begin(), t.end(), '8');
-                string::difference_type n9 = count(t.begin(), t.end(), '9');
-
-                int c1 = static_cast<int>(n1);
-                int c2 = static_cast<int>(n2);
-                int c3 = static_cast<int>(n3);
-                int c4 = static_cast<int>(n4);
-                int c5 = static_cast<int>(n5);
-                int c6 = static_cast<int>(n6);
-                int c7 = static_cast<int>(n7);
-                int c8 = static_cast<int>(n8);
-                int c9 = static_cast<int>(n9);
-
-                if(c1>1||c2>1||c3>1||c4>1||c5>1||c6>1||c7>1||c8>1||c9>1)
-                    return false;
-            }
-        }
-
-        return true;
-    }
+    set<char> valid;
 
     void solveSudoku(vector<vector<char>>& board) 
     {
-        
+        vector<pair<int, int>> dots_list;
+
+        for(int i = 0; i < 9; i++)
+        {
+            for(int j = 0; j < 9; j++)
+            {
+                if(board[i][j] == '.')
+                {
+                    dots_list.push_back({i, j});
+                }
+            }
+        }
+
+        sort(dots_list.begin(), dots_list.end(), [&](pair<int, int> a, pair<int, int> b) {
+            return countValidNumbers(board, a.first, a.second) < 
+                countValidNumbers(board, b.first, b.second);
+        });
+
+        solve(board, dots_list, 0);
+    }
+
+    int countValidNumbers(vector<vector<char>>& board, int i, int j) {
+        bool available[10] = {true};
+        getValidNumbers(board, i, j, available);
+        return count(begin(available) + 1, end(available), true);
+    }
+
+    bool solve(vector<vector<char>> &board, vector<pair<int, int>> &dots, int index)
+    {
+        if(index == dots.size())
+            return true;
+
+        int i = dots[index].first;
+        int j = dots[index].second;
+        bool available[10] = {true};
+        getValidNumbers(board, i, j, available);
+
+        for(char n = '1'; n <= '9'; ++n)
+        {
+            if(available[n-'0'])
+            {
+                board[i][j] = n;
+                if(solve(board, dots, index+1))
+                    return true;
+                board[i][j] = '.';
+            }
+        }
+        return false;
+    }
+
+    void getValidNumbers(vector<vector<char>>& board, int i, int j, bool available[])
+    {
+        fill(available, available + 10, true);
+
+        for (int k = 0; k < 9; k++) {
+            if (board[i][k] != '.') available[board[i][k] - '0'] = false;
+            if (board[k][j] != '.') available[board[k][j] - '0'] = false;
+        }
+
+        int row_start = (i / 3) * 3, col_start = (j / 3) * 3;
+        for (int r = row_start; r < row_start + 3; r++) {
+            for (int c = col_start; c < col_start + 3; c++) {
+                if (board[r][c] != '.') available[board[r][c] - '0'] = false;
+            }
+        }
     }
 };
 
+void print(vector<vector<char>> &v)
+{
+    for(int i = 0; i < 9; i++)
+    {
+        for(int j = 0; j < 9; j++)
+        {
+            cout << v[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << "----------------" << endl;
+}
+
 int main(int argc, char const *argv[])
 {
-    vector<vector<char>> v{{'5','3','.','.','7','.','.','.','.'},{'6','.','.','1','9','5','.','.','.'},{'.','9','8','.','.','.','.','6','.'},{'8','.','.','.','6','.','.','.','3'},{'4','.','.','8','.','3','.','.','1'},{'7','.','.','.','2','.','.','.','6'},{'.','6','.','.','.','.','2','8','.'},{'.','.','.','4','1','9','.','.','5'},{'.','.','.','.','8','.','.','7','9'}};
+    vector<vector<char>> v;
+    v = {{'.','.','.','.','.','.','.','.','.'},{'.','9','.','.','1','.','.','3','.'},{'.','.','6','.','2','.','7','.','.'},{'.','.','.','3','.','4','.','.','.'},{'2','1','.','.','.','.','.','9','8'},{'.','.','.','.','.','.','.','.','.'},{'.','.','2','5','.','6','4','.','.'},{'.','8','.','.','.','.','.','1','.'},{'.','.','.','.','.','.','.','.','.'}};
+    v = {{'5','3','.','.','7','.','.','.','.'},{'6','.','.','1','9','5','.','.','.'},{'.','9','8','.','.','.','.','6','.'},{'8','.','.','.','6','.','.','.','3'},{'4','.','.','8','.','3','.','.','1'},{'7','.','.','.','2','.','.','.','6'},{'.','6','.','.','.','.','2','8','.'},{'.','.','.','4','1','9','.','.','5'},{'.','.','.','.','8','.','.','7','9'}};
     Solution s;
+
+    clock_t tStart = clock();
+    s.solveSudoku(v);
+
+    print(v);
+    printf("Time taken: %.2f s\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
 
     return 0;
 }
